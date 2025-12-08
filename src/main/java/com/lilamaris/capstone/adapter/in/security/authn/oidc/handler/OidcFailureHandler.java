6a06f9d676a35bec9cc7463251feb681e5b59a7d
lilamaris <lilamaris.dev@gmail.com>
2025-12-08 14:57:20 +0900

    add oidc authentication failure handler

---
+++ src/main/java/com/lilamaris/capstone/adapter/in/security/authn/oidc/handler/OidcFailureHandler.java
@@ -0,0 +1,24 @@
+package com.lilamaris.capstone.adapter.in.security.authn.oidc.handler;
+
+import com.lilamaris.capstone.adapter.in.security.util.ResponseWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OidcFailureHandler implements AuthenticationFailureHandler {
+    private final ResponseWriter writer;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        writer.sendError(response, HttpStatus.UNAUTHORIZED, exception.getMessage());
+    }
+}
