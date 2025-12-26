d93f4510cc6f7e988b54f7d3df92810ddb29735d
lilamaris <lilamaris.dev@gmail.com>
2025-12-26 19:47:59 +0900

    default canonical event payload

---
+++ src/main/java/com/lilamaris/capstone/domain/model/common/event/ResourceCreated.java
@@ -0,0 +1,13 @@
+package com.lilamaris.capstone.domain.model.common.event;
+
+import com.lilamaris.capstone.domain.model.common.event.actor.CanonicalActor;
+import com.lilamaris.capstone.domain.model.common.id.DomainRef;
+
+import java.time.Instant;
+
+public record ResourceCreated(
+        DomainRef ref,
+        CanonicalActor actor,
+        Instant occurredAt
+) implements CanonicalEvent {
+}
---
+++ src/main/java/com/lilamaris/capstone/domain/model/common/event/ResourceGranted.java
@@ -0,0 +1,14 @@
+package com.lilamaris.capstone.domain.model.common.event;
+
+import com.lilamaris.capstone.domain.model.common.event.actor.CanonicalActor;
+import com.lilamaris.capstone.domain.model.common.id.DomainRef;
+
+import java.time.Instant;
+
+public record ResourceGranted(
+        DomainRef ref,
+        CanonicalActor actor,
+        CanonicalActor to,
+        Instant occurredAt
+) implements CanonicalEvent {
+}
