f9f0ac63bd44610cc9d43a1dc11ef95ffe2087e6
lilamaris <lilamaris.dev@gmail.com>
2025-12-30 02:09:21 +0900

    fix id generation context usage

--- src/test/java/com/lilamaris/capstone/domain/AccessControlTest.java
+++ src/test/java/com/lilamaris/capstone/domain/AccessControlTest.java
@@ -2,6 +2,7 @@ package com.lilamaris.capstone.domain;
 
 import com.lilamaris.capstone.application.policy.identity.IdGenerationContext;
 import com.lilamaris.capstone.application.policy.identity.defaults.DefaultIdGenerationContext;
+import com.lilamaris.capstone.application.policy.identity.defaults.RawBasedIdGenerator;
 import com.lilamaris.capstone.domain.model.auth.access_control.AccessControl;
 import com.lilamaris.capstone.domain.model.auth.access_control.id.AccessControlId;
 import com.lilamaris.capstone.domain.model.capstone.timeline.id.TimelineId;
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -23,34 +25,24 @@ public class AccessControlTest {
     private static final Logger log = LoggerFactory.getLogger(AccessControlTest.class);
     private CanonicalActor actor;
     private Referenceable referenceable;
-
     private IdGenerationContext ids;
-    private Supplier<AccessControlId> accessControlIdSupplier;
-    private Supplier<TimelineId> timelineIdSupplier;
-    private Supplier<UserId> userIdSupplier;
-
 
     @BeforeEach
     void run() {
-        var registry = new DefaultIdGenerationContext();
-        var uuidRawGenerator = new SequentialUuidGenerator();
-        registry.register(AccessControlId.class, AccessControlId::new, uuidRawGenerator);
-        registry.register(TimelineId.class, TimelineId::new, uuidRawGenerator);
-        registry.register(UserId.class, UserId::new, uuidRawGenerator);
+        var timelineIdGen = new RawBasedIdGenerator<>(TimelineId.class, TimelineId::new, new SequentialUuidGenerator());
+        var accessControlIdGen = new RawBasedIdGenerator<>(AccessControlId.class, AccessControlId::new, new SequentialUuidGenerator());
+        var userIdGen = new RawBasedIdGenerator<>(UserId.class, UserId::new, new SequentialUuidGenerator());
 
-        ids = registry;
-        accessControlIdSupplier = () -> ids.next(AccessControlId.class);
-        timelineIdSupplier = () -> ids.next(TimelineId.class);
-        userIdSupplier = () -> ids.next(UserId.class);
+        ids = new DefaultIdGenerationContext(List.of(timelineIdGen, accessControlIdGen, userIdGen));
 
-        actor = UserActor.of(userIdSupplier.get());
-        referenceable = timelineIdSupplier.get();
+        actor = UserActor.of(ids.next(UserId.class).get());
+        referenceable = ids.next(TimelineId.class).get();
     }
 
     @Test
     void should_create() {
         var ref = referenceable.ref();
-        var accessControl = AccessControl.create(actor, ref, "test", accessControlIdSupplier);
+        var accessControl = AccessControl.create(actor, ref, "test", ids.next(AccessControlId.class));
 
         log.info("AccessControl: {}", accessControl);
         assertThat(accessControl.getActor().sameIdentity(actor)).isTrue();
