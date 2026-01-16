f986563a54695b7b670ae6784c432ff58bb6dec8
lilamaris <lilamaris.dev@gmail.com>
2026-01-16 15:37:25 +0900

    add json patch contract and default implementation

---
+++ src/main/java/com/lilamaris/capstone/shared/application/support/DefaultJsonPatchEngine.java
@@ -0,0 +1,42 @@
+package com.lilamaris.capstone.shared.application.support;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DefaultJsonPatchEngine implements JsonPatchEngine {
+    private final ObjectMapper mapper;
+
+    @Override
+    public <T> T fromPatch(String patch, Class<T> clazz) {
+        try {
+            var patchNode = mapper.readTree(patch);
+            return mapper.treeToValue(patchNode, clazz);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to apply patch: ", e);
+        }
+    }
+
+    @Override
+    public <T> T apply(String patch, T target, Class<T> clazz) {
+        try {
+            var jsonPatch = JsonPatch.fromJson(mapper.readTree(patch));
+            var patched = jsonPatch.apply(mapper.valueToTree(target));
+            return mapper.treeToValue(patched, clazz);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to apply patch: ", e);
+        }
+    }
+
+    @Override
+    public <T> String createPatch(T target) {
+        try {
+            return mapper.writeValueAsString(target);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed create patch: ", e);
+        }
+    }
+}
---
+++ src/main/java/com/lilamaris/capstone/shared/application/support/JsonPatchEngine.java
@@ -0,0 +1,9 @@
+package com.lilamaris.capstone.shared.application.support;
+
+public interface JsonPatchEngine {
+    <T> T fromPatch(String patch, Class<T> clazz);
+
+    <T> T apply(String patch, T target, Class<T> clazz);
+
+    <T> String createPatch(T target);
+}
