9f1988977880e0326f2895bf3848132d81abb654
lilamaris <lilamaris.dev@gmail.com>
2026-01-16 12:52:38 +0900

    add dependency for sql log

--- src/main/java/com/lilamaris/capstone/shared/application/config/ApplicationConfig.java
+++ src/main/java/com/lilamaris/capstone/shared/application/config/ApplicationConfig.java
@@ -3,8 +3,12 @@ package com.lilamaris.capstone.shared.application.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+import java.time.ZoneId;
 
 @Configuration
 public class ApplicationConfig {
@@ -14,5 +18,15 @@ public class ApplicationConfig {
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
+    }
+
+    @Bean
+    ZoneId zoneId(@Value("${university.timezone}") String zone) {
+        return ZoneId.of(zone);
+    }
+
+    @Bean
+    Clock clock(ZoneId zoneId) {
+        return Clock.system(zoneId);
     }
 }
