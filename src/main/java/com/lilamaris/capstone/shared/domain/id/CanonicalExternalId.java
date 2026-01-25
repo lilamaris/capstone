e4e39ac449d93246777958b47907b5a1d5566caa
lilamaris <lilamaris.dev@gmail.com>
2026-01-25 21:54:40 +0900

    add externalizable convert

--- src/main/java/com/lilamaris/capstone/shared/domain/id/CanonicalExternalId.java
+++ src/main/java/com/lilamaris/capstone/shared/domain/id/CanonicalExternalId.java
@@ -1,9 +1,15 @@
 package com.lilamaris.capstone.shared.domain.id;
 
+import com.lilamaris.capstone.shared.domain.defaults.DefaultExternalizableId;
+
 public record CanonicalExternalId(
         String value
 ) {
     public static CanonicalExternalId from(ExternalizableId externalId) {
         return new CanonicalExternalId(externalId.asString());
+    }
+
+    public ExternalizableId toExternalizableId() {
+        return new DefaultExternalizableId(value);
     }
 }
