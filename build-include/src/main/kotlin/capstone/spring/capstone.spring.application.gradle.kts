afd3cf5a3fb0a01f64075105464987b561f3f60a
lilamaris <lilamaris.dev@gmail.com>
2026-04-29 22:31:53 +0900

    refactor(build): 웹 계층용 convention plugin 개선

--- build-include/src/main/kotlin/capstone/spring/capstone.spring.application.gradle.kts
+++ build-include/src/main/kotlin/capstone/spring/capstone.spring.application.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
-    id("capstone.spring.platform")
+    id("capstone.spring.module")
+}
+
+val libs = extensions
+    .getByType<VersionCatalogsExtension>()
+    .named("libs")
+
+dependencies {
+
 }
