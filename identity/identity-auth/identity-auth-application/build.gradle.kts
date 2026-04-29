c9fb8b924c1241382588157c07e239e0de7e2814
lilamaris <lilamaris.dev@gmail.com>
2026-04-30 08:32:41 +0900

    feat(identity-auth-application): application 모듈 구성

---
+++ identity/identity-auth/identity-auth-application/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("capstone.layer.application")
+}
+
+group = "com.lilamaris.capstone"
+version = "0.0.1-SNAPSHOT"
+
+dependencies {
+    implementation(project(":identity:identity-auth:identity-auth-domain"))
+
+    implementation(project(":identity:identity-core"))
+    implementation(project(":kernel:kernel-core"))
+
+    implementation(libs.spring.security.crypto)
+
+    testImplementation(testFixtures(project(":identity:identity-auth:identity-auth-domain")))
+    testImplementation(testFixtures(project(":kernel:kernel-core")))
+    testImplementation(project(":kernel:kernel-test-support"))
+}
