614cb82a3db0299217ecfd595b8b01ad45b2c599
lilamaris <lilamaris.dev@gmail.com>
2026-04-29 22:20:53 +0900

    refactor(build): 애플리케이션 계층용 convention plugin 개선

---
+++ build-include/src/main/kotlin/capstone/layer/capstone.layer.application.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("capstone.spring.application")
+}
+
+val libs = extensions
+    .getByType<VersionCatalogsExtension>()
+    .named("libs")
+
+dependencies {
+    add("implementation", libs.findLibrary("slf4j-api").get())
+}
