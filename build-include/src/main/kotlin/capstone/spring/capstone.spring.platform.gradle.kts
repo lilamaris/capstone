fca00032e0768dfa63269c84fced6a6571ddc11c
lilamaris <lilamaris.dev@gmail.com>
2026-04-29 22:31:53 +0900

    refactor(build): 웹 계층용 convention plugin 개선

--- build-include/src/main/kotlin/capstone/spring/capstone.spring.platform.gradle.kts
+++ build-include/src/main/kotlin/capstone/spring/capstone.spring.platform.gradle.kts
@@ -8,6 +8,7 @@ val libs = extensions
 
 dependencies {
     add("implementation", platform(libs.findLibrary("spring-boot-dependencies").get()))
+    add("implementation", libs.findLibrary("slf4j-api").get())
 
     add("testImplementation", platform(libs.findLibrary("spring-boot-dependencies").get()))
 }
