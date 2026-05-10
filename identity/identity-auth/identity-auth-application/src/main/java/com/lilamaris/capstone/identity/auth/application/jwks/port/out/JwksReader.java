4f6e04a59a5056dda7bca9a51d20c7cbefe94630
lilamaris <lilamaris.dev@gmail.com>
2026-05-10 10:55:50 +0900

    fix(identity-auth-application): JwksReader 스펙 변경

--- identity/identity-auth/identity-auth-application/src/main/java/com/lilamaris/capstone/identity/auth/application/jwks/port/out/JwksReader.java
+++ identity/identity-auth/identity-auth-application/src/main/java/com/lilamaris/capstone/identity/auth/application/jwks/port/out/JwksReader.java
@@ -1,12 +1,11 @@
 package com.lilamaris.capstone.identity.auth.application.jwks.port.out;
 
-import com.lilamaris.capstone.identity.auth.domain.jwks.RSASignatureKey;
 import com.lilamaris.capstone.identity.auth.domain.jwks.RSAVerificationKey;
 
 import java.util.List;
 
 public interface JwksReader {
-    RSASignatureKey findSignableKey();
+    KeyMaterial findSignableKey();
 
     List<RSAVerificationKey> findVerifiableKeys();
 }
