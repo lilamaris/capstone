bfb01e498a71f14452d1e7734f9789d9d63e6459
lilamaris <lilamaris.dev@gmail.com>
2026-05-20 03:36:28 +0900

    feat(academic-catalog-domain): 구간 관계 열거형 질의 메서드 추가

--- academic-catalog/academic-catalog-domain/src/main/java/com/lilamaris/capstone/academiccatalog/domain/shared/time/RangeRelation.java
+++ academic-catalog/academic-catalog-domain/src/main/java/com/lilamaris/capstone/academiccatalog/domain/shared/time/RangeRelation.java
@@ -9,5 +9,41 @@ public enum RangeRelation {
     IMMEDIATELY_BEFORE,
     IMMEDIATELY_AFTER,
     OVERLAPS_AFTER,
-    OVERLAPS_BEFORE
+    OVERLAPS_BEFORE;
+
+    public boolean isSame() {
+        return this == SAME;
+    }
+
+    public boolean isContains() {
+        return this == CONTAINS;
+    }
+
+    public boolean isContainedBy() {
+        return this == CONTAINED_BY;
+    }
+
+    public boolean isBefore() {
+        return this == BEFORE;
+    }
+
+    public boolean isAfter() {
+        return this == AFTER;
+    }
+
+    public boolean isImmediatelyBefore() {
+        return this == IMMEDIATELY_BEFORE;
+    }
+
+    public boolean isImmediatelyAfter() {
+        return this == IMMEDIATELY_AFTER;
+    }
+
+    public boolean isOverlapAfter() {
+        return this == OVERLAPS_AFTER;
+    }
+
+    public boolean isOverlapBefore() {
+        return this == OVERLAPS_BEFORE;
+    }
 }
